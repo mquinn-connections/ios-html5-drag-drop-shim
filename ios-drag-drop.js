4440ebb7db8922a1551462b0c91f4c96c6b38f7b
Michael Quinn <mquinn@connectionseducation.com>
2016-09-21 15:59:07 -0400

    Changes.

--- ios-drag-drop.js
+++ ios-drag-drop.js
@@ -311,18 +311,7 @@
   }
 
   function touchend(evt) {
-    var el = evt.target;
-
-    if(dragging) {
-      dragging = false;
-    } else {
-      var clickEvt = document.createEvent("MouseEvents");
-      clickEvt.initMouseEvent("click", true, true, el.ownerDocument.defaultView, 1,
-          evt.screenX, evt.screenY, evt.clientX, evt.clientY,
-          evt.ctrlKey, evt.altKey, evt.shiftKey, evt.metaKey, 0, null);
-      el.dispatchEvent(clickEvt);
-      log("Simulating click to anchor");
-    }
+    dragging = false;
   }
 
   // DOM helpers
